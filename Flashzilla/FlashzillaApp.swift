36b3fa5adc49eb5afdecfa5b595d7f12de130d78
Mberndt92 <maximilian_berndt@icloud.com>
2023-05-05 08:37:49 +0900

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Maximilian Berndt on 2023/05/05.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
